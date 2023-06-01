3d575a3094014d5553d2feb5a9940aa87e5b2501
Rene <0228505@up.edu.mx>
2023-05-31 18:04:30 -0600

    To do folders

---
+++ 7. Playing with transition/js/main.js
@@ -0,0 +1,62 @@
+/*
+*    main.js
+*/
+
+function random_rgba() {
+	var o = Math.round, r = Math.random, s = 255;
+	return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
+}
+
+function events() {
+	d3.json("data/revenues.json").then((data) => {
+
+		data.forEach((d) => {
+
+			d.revenue = +d.revenue;
+
+		});
+
+		var bars = g.selectAll("rect")
+
+			.data(data)
+
+			.enter()
+
+			.append("rect")
+
+			.attr("x", (d) => {
+
+				return x(d.month);
+
+			})
+
+			.attr("y", (d) => { return y(d.revenue); })
+
+			.attr("width", x.bandwidth)
+
+			.attr("height", (d) => {
+
+				return height - y(d.revenue);
+
+			})
+
+			.attr("fill", "yellow");
+
+			d3.interval(( ) => {
+
+				var newData = flag ? data : data.slice(1);
+			
+				update(newData);
+			
+				flag = !flag;
+			
+			}, 1000);
+
+	}).catch((error) => {
+
+		console.log(error);
+
+	});
+
+}
+events();
