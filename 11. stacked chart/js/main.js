035634098d02e1b0afe92f554a74dd238ecc4aac
Rene <0228505@up.edu.mx>
2023-05-31 19:01:52 -0600

    JUST IN TIME

--- 11. stacked chart/js/main.js
+++ 11. stacked chart/js/main.js
@@ -33,7 +33,13 @@ var yAxisCall = d3.axisLeft().tickFormat(formatBillion);
 // TODO: create the area generator. 
 // The x coordinate will be the date of the data
 // while y0 and y1 will be in the 0 and 1 positions of the d element
+let area = d3.area()
+    .x(d => x(d.data.date))
+    .y0(d => y(d[0]))
+    .y1(d => y(d[1]));
+
 // TODO: create the stack
+var stack = d3.stack();
 
 // Axis groups
 var xAxis = g.append("g")
@@ -64,7 +70,7 @@ d3.csv('data/stacked_area2.csv').then((data) => {
     }));
         
     //TODO: obtain the keys array, remember to remove the first column
-    var keys = [];
+    var keys = data.columns.filter((key) => { return key !== 'date'; });
 
 	data.forEach((d) => {
 	    d.date = parseDate(d.date);
@@ -88,13 +94,49 @@ d3.csv('data/stacked_area2.csv').then((data) => {
     // TODO: finish the configuration of the stack object
     // by setting the keys, order and offset
 
+    stack.keys(keys)
+        .order(d3.stackOrderNone)
+	    .offset(d3.stackOffsetNone);
+
     // TODO: bind the data to the stack and create the group 
     // that will contain the area path
 
+    let browser = g.selectAll('.browser')
+	    .data(stack(data))
+	    .enter().append('g')
+	    .attr('class', d => { return 'browser ' + d.key; })
+	    .attr('fill-opacity', 0.5);
+
     // TODO: call the area generator with the appropriate data
+
+    browser.append('path')
+	    .attr('class', 'area')
+	    .attr('d', area)
+	    .style('fill', d => color(d.key));
 
     // Create legend
     // TODO: Create a legend showing all the names of every color
+    let legend = g.append("g")
+        .attr("transform", "translate(" + (width + 150) + "," + (height - 210) + ")")
+        .attr("fill", "black");
+
+    const reverseK = keys.slice();
+
+    for (let i = 0; i < reverseK.length; i++){
+        
+        let line = legend.append("g").attr("transform", "translate(0, " + i * 20 + ")");
+
+        line.append("rect")
+            .attr("width", 10)
+            .attr("height", 10)
+            .attr("fill", color(reverseK[i]));
+
+        line.append("text")
+            .attr("x", -10)
+            .attr("y", 10)
+            .attr("text-anchor", "end")
+            .text(reverseK[i]);
+    }
 
 }).catch((error) => {
     console.log(error);
